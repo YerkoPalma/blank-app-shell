95be5b36647177f714bdf30263efdcb6924acca3
Yerko Palma <yerko.palma@usach.cl>
2016-12-05 17:46:29 +0000

    try to toggle

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,5 +1,9 @@
 import yo from 'yo-yo'
+import { fadeOut } from '../utils'
 
 export default yo`
-<div class='absolute w-100 h-100 cover bg-black-60 o-0'>
+<div onclick=${(ev) => {
+      fadeOut(ev.target)
+      document.getElementById('header').style.opacity = '1'
+    }} class='absolute w-100 h-100 cover bg-black-60 o-0'>
 </div>`
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export function fadeIn (el) {
-  // el.style.opacity = 0
+  el.style.opacity = 0
 
   var last = +new Date()
   var tick = function () {
@@ -8,6 +8,25 @@ export function fadeIn (el) {
 
     if (+el.style.opacity < 1) {
       (window.requestAnimationFrame && window.requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  }
+
+  tick()
+}
+
+export function fadeOut (el) {
+  el.style.opacity = 1
+
+  var last = +new Date()
+  var tick = function () {
+    el.style.opacity = +el.style.opacity - (new Date() - last) / 400
+    last = +new Date()
+
+    if (+el.style.opacity > 0) {
+      (window.requestAnimationFrame && window.requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      // after changing opacity, actually hide the element
+    } else {
+      el.style.display = 'none'
     }
   }
 
