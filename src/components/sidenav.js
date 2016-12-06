321c103edf02f624156cb843cf29037dbcc9da28
Yerko Palma <yerko.palma@usach.cl>
2016-12-06 19:24:48 +0000

    added nav links

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -5,8 +5,22 @@ export default yo`
 <div>
   <aside onload=${function (e) { e.style.left = '-25%' }} id='sidenav' class='absolute h-100 w-25 bg-white shadow-3 left-0 top-0 z-2'>
     <div style='background-image: url(src/images/green-bg.png)' class='absolute cover w-100 h4'>
-      <h2 class='white'>App Shell</h2>
+      <h2 class='white w-80 tl pl3 mt5'>App Shell</h2>
     </div>
+    <ul class='absolute top-2 mt6 list light-silver f2'>
+      <li class='flex flex-row flex-wrap justify-start content-center items-center mv3'>
+        <i class="material-icons">info_outline</i> 
+        <a href="#" class="link f4 silver hover-dark-green self-center ml2">Home</a>
+      </li>
+      <li class='flex flex-row flex-wrap justify-start content-center items-center mv3'>
+        <i class="material-icons">info_outline</i> 
+        <a href="#" class="link f4 silver hover-dark-green self-center ml2">About</a>
+      </li>
+      <li class='flex flex-row flex-wrap justify-start content-center items-center mv3'>
+        <i class="material-icons">info_outline</i> 
+        <a href="#" class="link f4 silver hover-dark-green self-center ml2">Contact</a>
+      </li>
+    </ul>
   </aside>
   <div id='sidenav-panel' onclick=${(ev) => {
     fadeOut(ev.target)
